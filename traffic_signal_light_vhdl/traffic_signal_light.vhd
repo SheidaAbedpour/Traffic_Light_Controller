ced10c6c14e7cdad3daee9356953411e83857c37
Sheida Abedpour <sheida.abedpour2002@gmail.com>
2023-12-12 19:53:48 +0330

    initial commit

---
+++ traffic_signal_light_vhdl/traffic_signal_light.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    19:51:28 12/12/2023 
+-- Design Name: 
+-- Module Name:    traffic_signal_light - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity traffic_signal_light is
+end traffic_signal_light;
+
+architecture Behavioral of traffic_signal_light is
+
+begin
+
+
+end Behavioral;
+
